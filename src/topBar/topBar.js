13f48fcec0585e3bbd4f88d72d4148f8ee2b32e3
yashhsm <yashhsm@gmail.com>
2019-02-24 15:05:30 +0530

    added about

--- src/topBar/topBar.js
+++ src/topBar/topBar.js
@@ -8,7 +8,8 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import logo from './hawk-bird-animal-shape.svg';
 import blue from '@material-ui/core/colors/blue';
-
+import Git_icon from './Github_logo.svg';
+import Button from '@material-ui/core/Button';
 const color = blue[800];
 const styles = {
   root: {
@@ -16,6 +17,20 @@ const styles = {
   },
   
 };
+const styles1 = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit,
+  },
+  iconSmall: {
+    fontSize: 25,
+  },
+});
 
 function SimpleAppBar(props) {
   const { classes } = props;
@@ -29,8 +44,15 @@ function SimpleAppBar(props) {
         <img src={logo} id="logo"  alt="logo" />
                   
           <h4 id="main-title">
-            HawkEye
+            <b></b> Project HawkEye
           </h4>
+
+
+          <Button variant="contained" color="secondary" className={classes.button}>
+        
+        <img src={Git_icon} alt="Git_hub" height="30" width="30" />
+      </Button>
+        
         </Toolbar>
       </AppBar>
     </div>
@@ -41,7 +63,9 @@ SimpleAppBar.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
+
 export default withStyles(styles)(SimpleAppBar);
+
 
 
 
